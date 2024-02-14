ea51cc341298db85846f249565a7293f0cbfcdcb
Nshatil <43738501+Nshatil@users.noreply.github.com>
2024-02-14 10:52:02 +0000

    mod

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -35,3 +35,44 @@ accountCity="Banglaru"
 
 console.log(accountId,accountEmail,accountPassword,accountCity);
 console.table([accountId,accountEmail,accountPassword,accountCity,accuntState]);
+
+// Exploring var,let const using block scope and functional scope.
+
+/*
+
+var => is a functional scope
+let,cons=> is a block scope
+
+*/
+
+
+// writing a fuction hello which will include these variables
+
+function hello()
+{
+
+if(true){
+
+var a= "JS"
+
+let b="Java"
+
+const c= "C++"
+
+console.log("inside condigtional staement!");// block scope
+console.log(a);
+console.log(b); //all output ok
+console.log(c);
+
+
+}
+
+console.log(" outside condigtional staement!");// fucntional scope
+console.log(a);
+console.log(b); //error
+console.log(c);//error 
+//b&c both is call outside from the block scope
+
+}
+
+hello();// caling the function
