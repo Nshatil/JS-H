4e77fe8902681644e2c29ba6a3e0bd809e3e9dc9
Nshatil <43738501+Nshatil@users.noreply.github.com>
2024-02-14 09:01:38 +0000

    exploring JS variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,37 @@
+//exploring variables of js and investigating...
+
+//investigory coding
+//const, let , var;
+const accountId =23456
+let accountEmail = "shatl3434@gmail.com"
+
+var accountPassword = "21345"
+
+accountCity="Jaipur"
+
+// added a new varialble
+
+let accuntState="mr."; // output:undefined! unless assign a value.
+
+
+//accountId change not allowed.
+
+/*
+Preffered not to use var because of issue
+block scope and functional scope
+*/
+
+/*
+var is functional scope
+
+let, const block scope
+*/
+
+accountEmail="shatil873" 
+accountPassword="12345"
+
+accountCity="Banglaru"
+
+
+console.log(accountId,accountEmail,accountPassword,accountCity);
+console.table([accountId,accountEmail,accountPassword,accountCity,accuntState]);
