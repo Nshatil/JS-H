6c0867c6beae1a7b65952fef4d850688206c7516
Nshatil <43738501+Nshatil@users.noreply.github.com>
2024-02-16 21:37:13 +0000

    using push/pop

---
+++ 01_basics/practice.js
@@ -0,0 +1,19 @@
+//arry diclarare
+
+let arr=[12,23,323]
+
+//// checking the length
+
+//console.log(arr.indexOf)
+
+//console.log("push",arr.push("123"));
+//console.log("after push",arr.length);
+//console.log("new added index", arr[3],)
+
+//push returns the total length of the array after executing.
+
+//--------------- pop up method ------------
+
+console.log("using pop method",arr.pop());
+
+console.log("after using pop method",arr.length);
